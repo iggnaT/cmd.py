700196fb40fd54eed3c68e9af98fa84ffa2c18ec
iggnaT <81556468+iggnaT@users.noreply.github.com>
2021-07-03 08:22:36 +0300

    passord system
    why am I doing this? - x, 2018

--- cmd.py
+++ cmd.py
@@ -1,6 +1,5 @@
 import os
 
-name = "user"
 path = os.path.join("C:\\Users\\Public\\cmd.py\\config\\", "config.cfg")
 
 def firstenter():
@@ -8,15 +7,34 @@ def firstenter():
         os.mkdir("C:\\Users\\Public\\cmd.py")
         os.mkdir("C:\\Users\\Public\\cmd.py\\config")
         name = input("hi there! what's your name (you will be able to change it any time): ")
+        password = input("password (if this field will be blank then you will have only to press enter when your password is asked): ")
         f = open(path, 'w')
         f.write(name)
+        f.write("," + password)
         f.close
     except:
         pass
 
+firstenter()
+
 def getname():
-    f = open("C:\\Users\\Public\\cmd.py\\config\\config.cfg", 'r')
-    return f.read()
+    f = open(path, 'r')
+    data = f.read().split(",", 1)
+    return data[0]
+
+def getpassword():
+    f = open(path, 'r')
+    data = f.read().split(",", 1)
+    return data[1]
+
+globalpassword = getpassword()
+globalname = getname()
+
+def checkpassword(userpassword):
+    if(userpassword == getpassword()):
+        return True
+    else:
+        return False
 
 def say(thing):
     print(thing)
@@ -24,16 +42,26 @@ def say(thing):
 def changename():
     newname = input("what name do you want: ")
     f = open(path, 'w')
-    f.write(newname)
+    f.write(newname + "," + globalpassword)
     f.close
+    globalname = newname
+
+def changepassword():
+    globalname = getname()
+    newpass = input("enter new password: ")
+    globalname = getname()
+    f = open(path, 'w')
+    f.write(globalname + "," + newpass)
+    f.close
+    globalname = getname()
 
 def whatsnew():
     print("added:")
-    print("user system")
-    print("whats new command")
+    print("password system")
+    print("new command chngpass")
 
 def cmdver():
-    print("cmd.py v0.0.3, open source project.")
+    print("cmd.py v0.0.4, open source project.")
     print("github link: https://github.com/iggnaT/cmd.py")
     print("check it often for new updates")
     print("type help to get help")
@@ -77,13 +105,14 @@ def help(): # todo: add more help xd
     print("ver - shows version")
     print("say - says what you type")
     print("chngname - changes name you specified on first launch of cmd.py")
+    print("chngpass - changes password")
     print("whnew - shows what has been added and what was removed")
 
 def cmdthing():
         firstenter()
-        name = getname()
+        globalname = getname()
 
-        cmd = input("cmd.py\\" + name + " > ")
+        cmd = input("cmd.py\\" + globalname + " > ")
 
         if(cmd == "cmd.py"):
             cmdver()
@@ -92,11 +121,17 @@ def cmdthing():
         if(cmd == "ver"):
             cmdver()
 
+        if(cmd == "whoami"):
+            print(name)
+            cmdthing()
+
         if(cmd == "say"):
             say(input("what to say: "))
+            cmdthing()
 
         if(cmd == "whnew"):
             whatsnew()
+            cmdthing()
 
         if(cmd == ""):
             cmdthing()
@@ -104,17 +139,47 @@ def cmdthing():
         try:
             if(cmd == "math"):
                 math(int(input("number 1: ")), input("what to do (+, -, * or /): "), int(input("number 2: ")))
+                cmdthing()
         except:
             print("error 5: some input fields are incorrect")
+            cmdthing()
             
         if(cmd == "\n"):
             cmdthing()
 
-        elif(cmd == "chngname"):
-            changename()
+        elif(cmd == "chngpass"):
+            abletodo = checkpassword(input("enter old password: "))
+            if(abletodo == True):
+                changepassword()
+                cmdthing()
+            else:
+                print("wrong password. try again. you have one attempt left")
+                abletodo = checkpassword(input("enter old password: "))
+                if(abletodo == True):
+                    changepassword()()
+                    cmdthing()
+                else:
+                    print("wrong password. try again later")
+                    cmdthing()
 
+        elif(cmd == "chngname"):
+            abletodo = checkpassword(input("password: "))
+            if(abletodo == True):
+                changename()
+                cmdthing()
+            else:
+                print("wrong password. try again. you have one attempt left")
+                abletodo = checkpassword(input("password: "))
+                if(abletodo == True):
+                    changename()
+                    cmdthing()
+                else:
+                    print("wrong password. try again later")
+                    cmdthing()
+                
         elif(cmd == "read"):
-            readfile(input("file name: "))            
+            readfile(input("file name: "))     
+            cmdthing()       
 
         elif(cmd == " "):
             cmdthing()
@@ -123,16 +188,42 @@ def cmdthing():
             exit()
 
         elif(cmd == "crt"):
-            create(input("file name: "), input("contents: "))
+            abletodo = checkpassword(input("password: "))
+            if(abletodo == True):
+                create(input("file name: "), input("contents: "))
+                cmdthing()
+            else:
+                print("wrong password. try again. you have one attempt left")
+                abletodo = checkpassword(input("password: "))
+                if(abletodo == True):
+                    create(input("file name: "), input("contents: "))
+                    cmdthing()
+                else:
+                    print("wrong password. try again later")
+                    cmdthing()
 
         elif(cmd == "dlt"):
-            delete(input("file name: "))
+            abletodo = checkpassword(input("password: "))
+            if(abletodo == True):
+                delete(input("file name: "))
+                cmdthing()
+            else:
+                print("wrong password. try again. you have one attempt left")
+                abletodo = checkpassword(input("password: "))
+                if(abletodo == True):
+                    delete(input("file name: "))
+                    cmdthing()
+                else:
+                    print("wrong password. try again later")
+                    cmdthing()
 
         elif(cmd == "help"):
             help()
+            cmdthing()
 
         else:
             print("wrong command. type help for help")
+            cmdthing()
 
 while True:
     cmdthing()
